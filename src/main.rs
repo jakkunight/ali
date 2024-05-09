0efd31f88e7d176ef8cf84efb2f5e3ef5e813be4
Jakku Night <J97k107u45k117n@gmail.com>
2024-05-09 13:34:29 -0400

    Add support for linear equations system solving.

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn menu_option() -> u8 {
     println!("(2) Matrix substraction");
     println!("(3) Matrix product");
     println!("(4) Matrix determinant");
+    println!("(5) Linear equation solver");
     println!("(0) Exit");
     input::<u8>(Some("Insert a valid option:".to_string()))
 }
@@ -96,6 +97,38 @@ fn find_determinant() {
     pause();
 }
 
+fn equation_solver() {
+    clear();
+    println!("###### LINEAR EQUATION SLOVER #######");
+    println!("Finds the solutions of a linear ");
+    println!("equation system by the Cramer's Rule.");
+    println!();
+    let variables = input::<usize>(Some("Input the number of variables:".to_string()));
+    println!("############ SYSTEM DELTA ###########");
+    let mut system_matrix = Matrix::new(variables, variables);
+    system_matrix.edit();
+    let system_delta = system_matrix.clone().determinant();
+    println!();
+    println!("######### EQUATIONS RESULTS #########");
+    println!("Set the system's equations results:");
+    println!();
+    let mut results = Matrix::new(variables, 1);
+    results.edit();
+    println!();
+    println!("########## SYSTEM SOLUTIONS ##########");
+    
+    for x in 0..variables {
+        let mut x_delta = system_matrix.clone();
+        for i in 0..variables {
+            x_delta.set(i, x, results.get(i, 0).unwrap());
+        }
+        let value = x_delta.determinant() / system_delta;
+        println!("Variable x{} = {}", x, value);
+    }
+
+    pause();
+}
+
 fn main() {
     loop {
         match menu_option() {
@@ -107,6 +140,7 @@ fn main() {
             2 => sub_matrixes(),
             3 => multiply_two_matrixes(),
             4 => find_determinant(),
+            5 => equation_solver(),
             _ => println!("INVALID OPTION"),
         }
     }
